684d2c68204df42d65afa56996038325e2ab2a82
Sam Roberts <sam@smrobtzz.dev>
2025-12-17 18:24:39 -0500

    remove

--- for-rust.js
+++
@@ -1,9 +0,0 @@
-export function listing_out(str) {
-  const listing = document.getElementById('listing');
-  listing.value += str;
-}
-
-export function object_out(str) {
-  const object = document.getElementById('object');
-  object.value += str;
-}
